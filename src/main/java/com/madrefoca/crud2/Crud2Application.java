3ce91ef57f2dc132fa4e74074d6e2d9a4fe2b3b7
fernando <Fernando.Gomez3@wnco.com>
2018-10-17 14:13:18 -0500

    initial commit

---
+++ src/main/java/com/madrefoca/crud2/Crud2Application.java
@@ -0,0 +1,12 @@
+package com.madrefoca.crud2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Crud2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Crud2Application.class, args);
+	}
+}
